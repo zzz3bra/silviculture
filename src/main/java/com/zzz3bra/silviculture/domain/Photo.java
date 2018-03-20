badbc509b421d72cffca452bb8bbd1cc28822620
Rodion Hrechukha <zzz3bra@gmail.com>
2018-03-21 00:00:28 +0300

    Fix for new field in photo

--- src/main/java/com/zzz3bra/silviculture/domain/Photo.java
+++ src/main/java/com/zzz3bra/silviculture/domain/Photo.java
@@ -1,8 +1,10 @@
 package com.zzz3bra.silviculture.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Photo {
     private String comment;
     private Images images;
