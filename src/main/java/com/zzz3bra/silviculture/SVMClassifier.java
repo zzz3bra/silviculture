f588d4f383e7ee1c56f93f4d1773da5eaa84395e
Rodion Hrechukha <zzz3bra@gmail.com>
2019-07-14 09:16:56 +0300

    Update dependencies less verbose toString of Ad

---
+++ src/main/java/com/zzz3bra/silviculture/SVMClassifier.java
@@ -0,0 +1,53 @@
+package com.zzz3bra.silviculture;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.classification.SVMModel;
+import org.apache.spark.mllib.classification.SVMWithSGD;
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.util.MLUtils;
+import scala.Tuple2;
+
+public class SVMClassifier {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("SVM Classifier Example").setMaster("local").set("spark.executor.memory","1g");
+        SparkContext sc = new SparkContext(conf);
+        String path = "mllib/sample_libsvm_data.txt";
+        JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(sc, path).toJavaRDD();
+
+        // Split initial RDD into two... [60% training data, 40% testing data].
+        JavaRDD<LabeledPoint> training = data.sample(false, 0.6, 11L);
+        training.cache();
+        JavaRDD<LabeledPoint> test = data.subtract(training);
+
+        // Run training algorithm to build the model.
+        int numIterations = 100;
+        final SVMModel model = SVMWithSGD.train(training.rdd(), numIterations);
+
+        // Clear the default threshold.
+        model.clearThreshold();
+
+        // Compute raw scores on the test set.
+        JavaRDD<Tuple2<Object, Object>> scoreAndLabels = test.map(
+                new Function<LabeledPoint, Tuple2<Object, Object>>() {
+                    public Tuple2<Object, Object> call(LabeledPoint p) {
+                        Double score = model.predict(p.features());
+                        return new Tuple2<Object, Object>(score, p.label());
+                    }
+                }
+        );
+
+        // Get evaluation metrics.
+        BinaryClassificationMetrics metrics =
+                new BinaryClassificationMetrics(JavaRDD.toRDD(scoreAndLabels));
+        double auROC = metrics.areaUnderROC();
+
+        System.out.println("Area under ROC = " + auROC);
+
+        // Save and load model
+//        model.save(sc, "myModelPath");
+//        SVMModel sameModel = SVMModel.load(sc, "myModelPath");
+    }
+}
